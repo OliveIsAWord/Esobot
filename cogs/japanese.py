193e33b76767b76e258c8858d01306a005686d91
Christina Hanson <christinahansondesu@gmail.com>
2023-11-29 23:14:09 +1300

    Loosen prompt

--- cogs/japanese.py
+++ cogs/japanese.py
@@ -103,6 +103,7 @@ class Japanese(commands.Cog):
         Your role is to explain references to Japanese culture and media, providing short but comprehensive descriptions in English.
         When given text written in Japanese, you give a literal translation of the text without saying anything else. Do not give further context or commentary.
         Responses should be 4 sentences long at most and preferably only one sentence.
+        You may do other tasks if told to explicitly.
     """.split())
 
     @staticmethod
